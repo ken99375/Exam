3e8af1bd69ae4bd88aceffd9754535108ef649ee
admin <admin@LAPTOP-52JMB8O5>
2025-04-22 10:28:48 +0900

    trycatchの追加

--- src/scoremanager/LoginAction.java
+++ src/scoremanager/LoginAction.java
@@ -1,8 +1,11 @@
 package scoremanager;
 
+import java.sql.Connection;
+
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import dao.Dao;
 import tool.Action;
 
 public class LoginAction extends Action {
@@ -10,7 +13,13 @@ public class LoginAction extends Action {
 	@Override
 	public void execute(HttpServletRequest req, HttpServletResponse res)
 			throws Exception {
+		try(Connection con = new Dao().getConnection()){
+			// JSPへforward
+			req.getRequestDispatcher("login.jsp").forward(req, res);
+		}catch(Exception e){
+			e.printStackTrace();
+		}
 		//JSPへフォワード 7
-		req.getRequestDispatcher("login.jsp").forward(req, res);
+//		req.getRequestDispatcher("login.jsp").forward(req, res);
 	}
 }
