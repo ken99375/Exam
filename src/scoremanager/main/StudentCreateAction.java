86171179af9e0969c36d6272640c84a60b14b827
T_ryoga <T_ryoga@192.168.40.20>
2025-04-24 02:10:02 +0900

    学生追加修正

--- src/scoremanager/main/StudentCreateAction.java
+++ src/scoremanager/main/StudentCreateAction.java
@@ -8,6 +8,8 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 
+import bean.Teacher;
+import dao.ClassNumDao;
 import tool.Action;
 
 public class StudentCreateAction extends Action{
@@ -17,7 +19,7 @@ public class StudentCreateAction extends Action{
 		// 入学年度は設定せずに今年度から５引いた値から１０個表示
 		try {
 			HttpSession session = req.getSession();
-
+			Teacher teacher = (Teacher)session.getAttribute("user");
 			//入学年度現在の年から-10と+10の合計20個から選択できるようにする
 			LocalDate todayDate = LocalDate.now(); // LocaDateインスタンスを取得
 			int year = todayDate.getYear(); // 現在の年を取得
@@ -28,7 +30,11 @@ public class StudentCreateAction extends Action{
 				entYearList.add(i);
 			}
 
-			session.setAttribute("entYearList", entYearList);
+			req.setAttribute("entYearList", entYearList);
+			// クラス番号一覧も取得
+		    ClassNumDao classNumDao = new ClassNumDao();
+		    List<String> classNums = classNumDao.filter(teacher.getSchool());
+		    req.setAttribute("class_num_set", classNums);
 			req.getRequestDispatcher("student_create.jsp").forward(req, res);
 		} catch (Exception e){
 			req.getRequestDispatcher("/error.jsp").forward(req, res);
