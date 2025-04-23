79f683b445efad8593b580dc6dca0a08fccb9d83
ken99375 <ktq2490316@stu.o-hara.ac.jp>
2025-04-23 21:42:48 +0900

    StudentUpdateExecuteAction、コメント付き

--- src/scoremanager/main/StudentUpdateExecuteAction.java
+++ src/scoremanager/main/StudentUpdateExecuteAction.java
@@ -12,51 +12,38 @@ import tool.Action;
 public class StudentUpdateExecuteAction extends Action {
 
   public void execute(HttpServletRequest req, HttpServletResponse res) throws Exception {
-
     req.setCharacterEncoding("UTF-8");
 
     // 入力値取得
-
+    // 回数
     String no = req.getParameter("no");
-
+    // 学生名
     String name = req.getParameter("name");
-
+    // 入学年度
     int entYear = Integer.parseInt(req.getParameter("entYear"));
-
+    // クラス番号
     String classNum = req.getParameter("classNum");
-
+    // 在学中フラグ
     boolean attend = req.getParameter("attend") != null;
 
     // セッションから先生を取得して、所属学校を取得
-
     Teacher teacher = (Teacher) req.getSession().getAttribute("user");
-
     School school = teacher.getSchool();
 
     // 学生情報をセット
-
     Student student = new Student();
-
     student.setNo(no);
-
     student.setName(name);
-
     student.setEntYear(entYear);
-
     student.setClassNum(classNum);
-
     student.setAttend(attend);
-
     student.setSchool(school);
 
     // 更新
-
     StudentDao dao = new StudentDao();
-
     dao.save(student);
 
     // 完了画面へ
-
     req.getRequestDispatcher("student_update_done.jsp").forward(req, res);
 
   }
