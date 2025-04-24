1deb03e018e8aa35309b2ff0bca08e5dd0becb8f
T_ryoga <T_ryoga@LAPTOP-7JQ8NUVS>
2025-04-24 10:16:43 +0900

    てすとリスト

--- src/scoremanager/main/TestListAction.java
+++ src/scoremanager/main/TestListAction.java
@@ -1,6 +1,8 @@
 package scoremanager.main;
 
 import java.io.IOException;
+import java.time.LocalDate;
+import java.util.ArrayList;
 import java.util.List;
 
 import javax.servlet.http.HttpServletRequest;
@@ -21,6 +23,8 @@ public class TestListAction extends Action{
 		 HttpSession session = req.getSession();
 		 Teacher teacher = (Teacher)session.getAttribute("user");
 
+		 LocalDate todaysDate = LocalDate.now(); // LcalDateインスタンスを取得
+		 int year = todaysDate.getYear(); // 現在の年を取得
 		 // ユーザ情報があった場合、→
 		 	// ユーザが所属しているクラスと科目データを取得する
 		 // ない場合はエラーメッセージを出す。
@@ -34,17 +38,19 @@ public class TestListAction extends Action{
 				SubjectDao subjectDao = new SubjectDao();
 				List<Subject> subjectList = subjectDao.filter(teacher.getSchool());
 
+				List<Integer> entYearSet = new ArrayList<>();
+				 // 10年前から1年後まで年をリストに追加
+				 for (int i = year -10; i < year + 10; i++) {
+					 entYearSet.add(i);
+				 }
+
 				// jspに渡すデータをセット(不明)
 				 req.setAttribute("classList", classlist);
 				 req.setAttribute("subjectList", subjectList);
-
+				 req.setAttribute("ent_year_set", entYearSet);
+				 req.getRequestDispatcher("test_list.jsp").forward(req,res);
 		 } else {
 			 req.setAttribute("error", "ユーザー情報が取得できませんでした。ログインし直してください。");
 		 }
-
-
-		 //
-		 req.getRequestDispatcher("test_list.jsp").forward(req,res);
-
 	}
 }
